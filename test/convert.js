688e2dcd4ffa66caf19ff2ce98dacf88c88538cc
Rick Craig <rick@spacerevolver.com>
2015-01-29 09:31:31 +0000

    Added tests

---
+++ test/convert.js
@@ -0,0 +1,14 @@
+var ConvertTiff = require('../lib/convert'),
+  should = require('chai').should;
+
+describe('Convert: #tiff-to-png', function(){
+
+  describe('From Array', function(){
+
+    it('Should initialise without any errors', function(){
+      var converter = new ConvertTiff();
+    });
+    
+  });
+
+});
